a6c47b66b55862c1f20724b4930020d6668789e9
kbeel <kbeel@KIEFER-PC>
2016-06-27 21:43:06 -0400

    Add project files.

---
+++ TokuCinema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TokuCinema.Startup))]
+namespace TokuCinema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
