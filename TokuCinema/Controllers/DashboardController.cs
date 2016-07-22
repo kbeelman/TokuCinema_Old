f69d41a8d6f75e39ce485926ce0345c50e785156
Joseph Bayes <joseph.w.bayes@outlook.com>
2016-07-22 16:01:25 -0700

    Ajaxed the Dashboard and improved some of the styling.
    Using the quick navigation tab pinned to the left, you can now navigate
    to all the index views without leaving the dashboard.

--- TokuCinema/Controllers/DashboardController.cs
+++ TokuCinema/Controllers/DashboardController.cs
@@ -13,5 +13,6 @@ namespace TokuCinema.Controllers
         {
             return View("Index");
         }
+        
     }
 }
---
+++ TokuCinema/Scripts/ajax-helpers.js
@@ -0,0 +1,17 @@
+﻿// Send the result of an ajax call to an element id
+    function ajaxMethodToElement(ActionName, ControllerName, ElementId, OptionalId) {
+        // Populate variable if an Id is submitted
+        if (OptionalId !== null || OptionalId !== "") {
+            var id = OptionalId;
+        }
+        
+        var urlString = "/" + ControllerName + "/" + ActionName + "?" + id;
+
+        $.ajax({
+            url: urlString,
+            data: { 'id': id },
+            success: function (result) {
+                $("#" + ElementId).html(result);
+            }
+        });
+    }
